4062a00772acb720a2093da81798656d00554fe9
mswiechowicz <maciek2ti@gmail.com>
2021-07-09 21:39:44 +0200

    Proxy set method update. A lot of bug fixes

--- oStore.js
+++ oStore.js
@@ -1,24 +1,35 @@
-const oStore = (store = {}, {rerender} = () => {console.warn("oStore needs rerender function.")}) => {
-    return new Proxy(store, {
+const oStore = (store = {}, instance = () => {console.warn("oStore needs rerender function.")}) => {
+    return new Proxy(store, handler(instance));
+}
+
+const handler = function(instance) {
+    return {
         deleteProperty: function (target, property) {
             delete target[property];
-            rerender();
+            instance?.rerender?.call(instance);
             return true;
         },
-        set: function (target, property, value, receiver) {
+        get: function (obj, prop) {
+            if (['[object Object]', '[object Array]'].indexOf(Object.prototype.toString.call(obj[prop])) > -1) {
+                return new Proxy(obj[prop], handler(instance));
+            }
+            return obj[prop];
+        },
+        set: function (target, property, value) {
             if (target[property] === value)
                 return true;
             target[property] = value;
             const waitTime = value === '' ? 300 : 0;
             debounce(()=>{
                 const focusHandler = document.activeElement.id;
-                rerender();
+                instance?.rerender?.call(instance);
                 document.getElementById(focusHandler)?.focus();
             },waitTime);
             return true;
         }
-    });
+    }
 }
+
 var executed = false;
 
 function debounce(func, wait) {
@@ -31,11 +42,11 @@ function debounce(func, wait) {
         if (!executed) {
             executed = true;
             func.apply(context, args);
-            // clearTimeout(executeTimeout);
+            clearTimeout(executeTimeout);
         }
     };
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
-};
+}
 
 export default oStore;
